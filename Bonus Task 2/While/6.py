c6204b25cfecdfa8714406a6cdac7b8791b7d37d
Sanzh.aub <sa_aubakirov@kbtu.kz>
2023-02-07 22:03:30 +0600

    Bonus Task 2

--- Bonus Task 2/While/6.py
+++ Bonus Task 2/While/6.py
@@ -1,9 +1,7 @@
 sum=0
-i=0
 while True:
     a=int(input())
     sum+=a
     if a==0:
         break
-    i+=1
-print(sum/i)
+print(sum)
--- Bonus Task 2/While/8.py
+++ Bonus Task 2/While/8.py
@@ -1,11 +1,9 @@
 max=0
-i=-1
 while True:
     a=int(input())
-    
     if a==0:
         break
     if a>max:
-        i+=1
         max=a
-print(i)
+print(max)
+
