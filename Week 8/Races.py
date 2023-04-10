c1324f5ac274595c1fd75f3062f0b17e71690dd8
Sanzh.aub <sa_aubakirov@kbtu.kz>
2023-04-10 16:42:51 +0600

    Week 8

--- Week 8/Races.py
+++ Week 8/Races.py
@@ -1,31 +1,30 @@
 import pygame, sys
 from pygame.locals import *
 import random, time
-# Инициализация
+# initialization
 pygame.init()
-# Настройка FPS
+# FPS
 FPS = 60
 FramePerSec = pygame.time.Clock()
-# Создание цветов
+# Creating colors
 BLUE = (0, 0, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
-# Другие переменные для использования в программе
+# Some variables
 SCREEN_WIDTH = 400
 SCREEN_HEIGHT = 600
 SPEEDENEMY = 5
 SPEEDCOIN = 6
-SPEEDCOIN2 = 5
 SCORE = 0
 NUMBERCOIN = 0
-# Настройка шрифтов
+# font settings
 font = pygame.font.Font('mp3/g.otf', 90)
 font_small = pygame.font.SysFont("Verdana", 20)
 game_over = font.render("Game Over", True, BLACK)
 background = pygame.image.load("images/AnimatedStreet.png")
-# Создайте белый экран
+# Create a white screen
 DISPLAYSURF = pygame.display.set_mode((400, 600))
 DISPLAYSURF.fill(WHITE)
 pygame.display.set_caption("Game")
@@ -35,17 +34,17 @@ class Enemy(pygame.sprite.Sprite):
         super().__init__()
         self.image = pygame.image.load("images/Enemy.png")
         self.rect = self.image.get_rect()
-        self.rect.center = (random.randint(40, SCREEN_WIDTH-40), 0) #gde ono budet poyiavitca
+        self.rect.center = (random.randint(40, SCREEN_WIDTH-40), 0) #point of occurrence
 
     def move(self):
-        #obivlyam score
+        #score
         global SCORE
-        #перемещает прямоугольник на место
+        #moves the rectangle into place
         self.rect.move_ip(0, SPEEDENEMY)
-        #esli enemy proshel ne kasaya player, togda score+=1
+        #If there was no collision
         if (self.rect.bottom > 600):
             SCORE += 1
-            #dalshe enemy poyiavliatsa randomno eshe raz
+            #random point of occurence
             self.rect.top = 0
             self.rect.center = (random.randint(40, SCREEN_WIDTH - 40), 0)
 #Setting Player
@@ -54,9 +53,9 @@ class Player(pygame.sprite.Sprite):
         super().__init__()
         self.image = pygame.image.load("images/Player.png")
         self.rect = self.image.get_rect()
-        self.rect.center = (160, 520) #gde ono budet poyiavitca
+        self.rect.center = (160, 520) #point of occurance
     def move(self):
-        #dvizheniya na pravo na levo,top,botom
+        #moves on right, left
         pressed_keys = pygame.key.get_pressed()
         if pressed_keys[K_UP]:
             self.rect.move_ip(0, -5)
@@ -75,78 +74,59 @@ class Coin(pygame.sprite.Sprite):
         self.image = pygame.image.load("images/coin.png")
         self.image = pygame.transform.scale(self.image, (30, 30))
         self.rect = self.image.get_rect()
-        self.rect.center = (random.randint(40, SCREEN_WIDTH - 40), 0)#gde ono budet poyiavitca
+        self.rect.center = (random.randint(40, SCREEN_WIDTH - 40), 0)
         self.state = False
 
     def move(self):
-        #obivlyam SPEEDCOIN
+        #SPEEDCOIN
         global SPEEDCOIN
-        #перемещает прямоугольник на место
+        #moves the rectangle into place
         self.rect.move_ip(0, SPEEDCOIN)
-        #poiyavlenya monet
+        #appearance of coins
         if (self.rect.bottom > 600 or self.state):
             SPEEDCOIN = 6
             self.state = False
             self.rect.top = 0
             self.rect.center = (random.randint(40, SCREEN_WIDTH - 40), 0)
-class Coin2(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        self.image = pygame.image.load("images/coin2.png")
-        self.image = pygame.transform.scale(self.image, (30, 30))
-        self.rect = self.image.get_rect()
-        self.rect.center = (random.randint(20, SCREEN_WIDTH - 60), 0)#gde ono budet poyiavitca
-        self.state = False
-
-    def move(self):
-        #obivlyam SPEEDCOIN
-        global SPEEDCOIN2
-        #перемещает прямоугольник на место
-        self.rect.move_ip(0, SPEEDCOIN2)
-        #poiyavlenya monet
-        if (self.rect.bottom > 600 or self.state):
-            SPEEDCOIN2 = 5
-            self.state = False
-            self.rect.top = 0
-            self.rect.center = (random.randint(20, SCREEN_WIDTH - 60), 0)
 # Setting up Sprites
 P1 = Player()
 E1 = Enemy()
-C1 = Coin()
-C2 = Coin2()
+C = Coin()
 # Creating Sprites Groups
 enemies = pygame.sprite.Group()
 enemies.add(E1)
 coins = pygame.sprite.Group()
-coins.add(C1)
-coins2 = pygame.sprite.Group()
-coins2.add(C2)
+coins.add(C)
 all_sprites = pygame.sprite.Group()
 all_sprites.add(P1)
 all_sprites.add(E1)
-all_sprites.add(C1)
-all_sprites.add(C2)
-# Игровая петля
+all_sprites.add(C)
+
+#Adding a new User event 
+INC_SPEED = pygame.USEREVENT + 1
+pygame.time.set_timer(INC_SPEED, 1000)
+
+# Game loop
 while True:
-    # Циклическое прохождение всех происходящих событий
     for event in pygame.event.get():
+        if event.type == INC_SPEED:
+              SPEEDENEMY += 0.5     
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
     #накладывать одно изображение на другое
     DISPLAYSURF.blit(background, (0, 0))
-    #контейнер для хранения и управления несколькими объектами Sprite
+    #container for storing and managing multiple Sprite objects
     scores = font_small.render(str(SCORE), True, BLACK)
     DISPLAYSURF.blit(scores, (10, 10))
     collect = font_small.render(str(NUMBERCOIN), True, BLACK)
     DISPLAYSURF.blit(collect, (SCREEN_WIDTH - 30, 10))
-    #Перемещает и перерисовывает все спрайты
+    #Moves and redraws all sprites
     for entity in all_sprites:
         DISPLAYSURF.blit(entity.image, entity.rect)
         entity.move()
-    # Выполняется, если происходит столкновение между игроком и противником
+    #It is executed if there is a collision between the player and the opponent
     if pygame.sprite.spritecollideany(P1, enemies):
-        #Простая проверка, пересекает ли спрайт что-либо в группе.
         pygame.mixer.Sound('mp3/crash.wav').play()
         time.sleep(1)
 
@@ -157,19 +137,13 @@ while True:
         pygame.display.update()
         for entity in all_sprites:
             entity.kill()
-            #удалить спрайт из всех групп
+            #remove sprite from all groups
         time.sleep(4)
         pygame.quit()
         sys.exit()
-
     if pygame.sprite.spritecollideany(P1, coins):
         NUMBERCOIN += 1
-        SPEEDENEMY += 1
         for entity in coins:
-            entity.state = True
-    if pygame.sprite.spritecollideany(P1, coins2):
-        NUMBERCOIN += 10
-        for entity in coins2:
             entity.state = True
     pygame.display.update()
     FramePerSec.tick(FPS)
--- Week 8/Snake.py
+++ Week 8/Snake.py
@@ -30,15 +30,15 @@ walls = { #уровни препятствий,
     3:[(0,0),(50,0),(100,0),(150,0),(200,0),(250,0),(300,0),(350,0),(400,0),(450,0),(500,0),(550,0),(600,0),(650,0),(700,0),(0,50),(0,100),(0,150),(0,200),(0,250),(0,300),(0,350),(0,400),(0,450),(0,500),(0,550),(0,600),(0,650),(0,700)]
 }
 
-speed = 5 #скорость для clock
-sup = 10 #для 42,43 а так же для 122 строки нужнo, для range чтобы узнать попадает ли score в range
-score = 2# очки за каждое яблоко
-dx,dy = 0,0 #направление 
+speed = 5 #sopeed for clock
+sup = 10 #for 42,43 as well as for 122 lines, it is necessary for range to find out whether score falls into range
+score = 2#points for each apple
+dx,dy = 0,0 #direction
 
 clock = pygame.time.Clock()
 time = 50 
 
-apple = randrange(50,mon,size),randrange(0,mon,size) #спавн яблока
+apple = randrange(50,mon,size),randrange(0,mon,size) #spawn of apple
 while apple in walls:
     apple = randrange(50,mon,size),randrange(0,mon,size)
 level1 = 9 #range 
@@ -51,12 +51,11 @@ score_font = pygame.font.SysFont("Verdana", 20)
 working = True
 while working:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:#vyhod iz progy
+        if event.type == pygame.QUIT:
             working = False
 
-        if event.type == pygame.KEYDOWN :# полностью движение 
-            if event.key == pygame.K_w and buttons['w']: # если нажали w то змейка будет двигать вверх, ибо dy будет равен -1(тоесть вы будете двигаться по координа dy в минусовыю часть, тоесть вверх)
-                #а buttonsp['w'] нужен для того чтобы при движений вниз s  мы не могли двигаться одновременно вверх
+        if event.type == pygame.KEYDOWN :# moves
+            if event.key == pygame.K_w and buttons['w']: #if you pressed w, the snake will move up, because dy will be equal to -1 (that is, you will move along the dy coordinate to the minus part, that is, up)
                 buttons = {
                     'w':True,
                     'a':True,
@@ -88,37 +87,37 @@ while working:
                     'd':True,
                     's':True #sw
                 }
-    screen.blit(bg,(0,0)) #рисуем задний фон
+    screen.blit(bg,(0,0)) #draw a font
 
-    if snake[-1] in walls[level]: # если координаты головы есть в координатах стен то игра останавливается
+    if snake[-1] in walls[level]: #if the coordinates of the head are in the coordinates of the walls, the game stops
         pygame.quit()
 
-    if apple in walls[level]:#нужна для проверки если яблоко появилось внутри стен
+    if apple in walls[level]:
         apple = randrange(50,mon,size),randrange(0,mon,size)
 
-    [(pygame.draw.rect(screen,pygame.Color('blue'),(i,j,size-2,size-2))) for i, j in snake]# рисуем змейку
+    [(pygame.draw.rect(screen,pygame.Color('blue'),(i,j,size-2,size-2))) for i, j in snake]#draw a snake
 
-    pygame.draw.rect (screen, pygame.Color('red'), (*apple, size-2,size-2))# рисуем яблоко
+    pygame.draw.rect (screen, pygame.Color('red'), (*apple, size-2,size-2))#draw an apple
 
-    for i in walls[level]:#рисуем стены определенного уровня
+    for i in walls[level]:#we draw walls of a certain level
         pygame.draw.rect(screen,(0,0,0),(i[0],i[1],48,48))
-    render_score = front.render(f'SCORE:{count}',1,pygame.Color('white'))#пишем score
-    screen.blit(render_score,(5,5))#рисуем score
+    render_score = front.render(f'SCORE:{count}',1,pygame.Color('white'))#
+    screen.blit(render_score,(5,5))#draw a score
 
-    if time == 0:#время для случая, если время для сьедания яблока истекло
+    if time == 0:#time for the case when the time for eating an apple has expired
         apple = randrange(50,mon,size),randrange(0,mon,size)
         time = 50
 
-    x += dx*size #непрерывное движение
+    x += dx*size #continuous movement
     y += dy*size
 
-    time -= 1 #раз в 50 speed мы обновляем местоположение яблоки, это нужно нам для 110 строки
+    time -= 1 #once every 50 speed we update the location of the apples, we need it for the 110 line
 
-    snake.append((x,y))#координаты змейки
+    snake.append((x,y))#coordinates of snake
    
-    snake = snake[-length:]#для того чтобы мы брали только змейку, а не места где она прошлась
+    snake = snake[-length:]#in order for us to take only the snake, and not the places where it walked
 
-    if count in range(level1,level2):#нужна для переноса в след уровень,а так же для увеличения награды за яблоко, скорость, 
+    if count in range(level1,level2):#it is needed to transfer to the next level, as well as to increase the reward for the apple, speed,
         level1 += sup
         level2 += sup
         sup += 20
@@ -128,13 +127,13 @@ while working:
         if level == 4:
             level = 1
 
-    if snake[-1] == apple: #в случае если координата яблоки и головы змейки совпала
+    if snake[-1] == apple: 
         count += score
         length += 1
         time = 50
         apple = randrange(50,mon,size),randrange(0,mon,size)
 
-    if x < 0 or x > mon-size or y < 0 or y > mon-size or len(snake) != len(set(snake)):#выход за пределы поля или змейка входит в себя
+    if x < 0 or x > mon-size or y < 0 or y > mon-size or len(snake) != len(set(snake)):#going out of bounds or the snake enters itself
         working = False
     pygame.display.flip()
     clock.tick(speed)
